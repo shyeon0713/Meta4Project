771a47f4e3182fdc0d32a6ec42143ebd13fb35e8
Yejin Lee <www298@naver.com>
2025-05-10 00:03:41 +0900

    main과 router, schema 분리. database password분리

---
+++ Server/database.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, HTTPException, Depends, status
+from typing import Annotated
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base  #이동시 models/base.py내부
+from Server.password import URL_DATABASE
+
+
+# engine 객체를 생성하면 SQLAlchemy가 해당 데이터베이스와 연결을 설정하고 SQL 명령을 실행할 수 있도록 한다.
+# 데이터베이스 연결 설정
+engine = create_engine(URL_DATABASE)
+
+# 세션 생성
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# ORM 모델 베이스 생성
+Base = declarative_base()  #이동시 models/base.py내부
+
+
+# 의존성 주입(Dependency Injection)을 위한 함수
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
