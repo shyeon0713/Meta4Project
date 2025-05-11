8e4098ec97aceac5ad278c62be0305b071250e72
bottle8429 <134829399+shyeon0713@users.noreply.github.com>
2025-05-11 13:05:00 +0900

    0511

--- Server/database.py
+++ Server/database.py
@@ -4,12 +4,17 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base  #이동시 models/base.py내부
-from Server.password import URL_DATABASE
+# from Server.password import URL_DATABASE
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv() #.env에 정의된 내용을 환경 변수로 등록
+DATABASE_URL = os.getenv("DATABASE_URL") #등록된 환경변수 중 하나를 가져옴
 
 # engine 객체를 생성하면 SQLAlchemy가 해당 데이터베이스와 연결을 설정하고 SQL 명령을 실행할 수 있도록 한다.
 # 데이터베이스 연결 설정
-engine = create_engine(URL_DATABASE)
+engine = create_engine(DATABASE_URL)
 
 # 세션 생성
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
